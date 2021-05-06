11e49f64b32030f7138fe18de4a795a4f47c0f79
Satomin2864 <satomiyuta0602@gmail.com>
2021-05-06 23:06:42 +0900

    echoを導入して動作確認

--- server.go
+++ server.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("start echo server")
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
