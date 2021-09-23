43dfd6d9bf37fda4b5856c78818b980d00f62908
Satomin2864 <satomiyuta0602@gmail.com>
2021-09-23 23:08:20 +0900

    環境変数からDBの情報を取得する関数を作成＆ORMを使ってdbに接続する機能をお試して作成

--- go.mod
+++ go.mod
@@ -2,4 +2,26 @@ module cocktail_api
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.2.2 // indirect
+require (
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/go-openapi/analysis v0.20.1 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/runtime v0.19.29 // indirect
+	github.com/go-swagger/go-swagger v0.27.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.2.2 // indirect
+	github.com/lib/pq v1.6.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
+	go.mongodb.org/mongo-driver v1.5.4 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/tools v0.1.4 // indirect
+	gorm.io/driver/postgres v1.1.0 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.11 // indirect
+)
--- server.go
+++ server.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	ID           uint
+	Name         string
+	Email        *string
+	Age          uint8
+	Birthday     time.Time
+	MemberNumber sql.NullString
+	ActivatedAt  sql.NullTime
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+type DBConf struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+}
 
 func main() {
 	fmt.Println("start echo server")
 	// Echo instance
 	e := echo.New()
-
+	// db_stady()
+	df, err := getDbEnv()
+	if err != nil {
+		println(err)
+	}
+	fmt.Println(df)
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -24,7 +54,50 @@ func main() {
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
+// envからDB関係の環境変数を取得する関数
+func getDbEnv() (df *DBConf, err error) {
+	// 環境変数を読み込み
+	err = godotenv.Load()
+
+	// エラーチェック
+	if err != nil {
+		return nil, err
+	}
+
+	// DBの各種変数を環境変数から取得
+	df = &DBConf{
+		Host:     os.Getenv("HOST"),
+		Port:     os.Getenv("PORT"),
+		User:     os.Getenv("USER"),
+		Password: os.Getenv("PASSWORD"),
+	}
+	// チェック
+	// fmt.Printf("HOST = %s, PORT = %s, USER = %s, PASSWORD = %s", df.Host, df.Port, df.User, df.Password)
+	// print(df)
+	return df, nil
+}
+
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+// gormを使ってdbの接続をする関数
+// SQL+プリペアードステートメントを利用する予定なので、最終的には削除する予定
+func db_stady() {
+	dsn := "host=localhost user=test password=test dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		print(err)
+	}
+	db.AutoMigrate(&User{})
+	user := User{Name: "Jinzhu", Age: 18, Birthday: time.Now()}
+
+	result := db.Create(&user)
+	println("[stmn] db status")
+	println(user.ID)
+	println(result.Error)
+	println((result.RowsAffected))
+	// result = db.Find(&user).Row()
+	println(result.Scan(&user))
 }
