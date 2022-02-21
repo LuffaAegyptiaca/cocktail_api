dd128288891b9a4a59fa803c1214ebd35ebcb9e4
Satomin2864 <satomiyuta0602@gmail.com>
2022-02-21 22:45:39 +0900

    docker-compseのinit側で定義しているので、こちらは削除

--- sql/tables/postgres/init/cocktail_recipe.sql
+++
@@ -1,7 +0,0 @@
-create table cocktail_recipe (
-    id              serial   PRIMARY KEY,
-    name            text     NOT NULL,
-    recipe          text     NOT NULL,
-    cocktail_style  text     NOT NULL,
-    alcohol         smallint NOT NULL
-);
