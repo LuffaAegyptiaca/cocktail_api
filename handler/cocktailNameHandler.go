3ac132ae5ba5dc46090b0f60d55f5a86eb773be9
Satomin2864 <satomiyuta0602@gmail.com>
2022-03-21 21:53:46 +0900

    カクテルレシピを全件習得するハンドラを記述

--- handler/cocktailNameHandler.go
+++ handler/cocktailNameHandler.go
@@ -10,23 +10,28 @@ import (
 )
 
 func GetCocktailsIngredient(c echo.Context) error {
+	// DB接続~クエリ実行まで
 	db := database.Connect()
 	defer db.Close()
 	rows, err := db.Query("SELECT * FROM cocktail_recipe")
-
 	if err != nil {
 		fmt.Println("err: ", err)
 	}
-	var cim_l []models.CocktailRecipes
+
+	resData := map[string][]models.CocktailRecipes{}
+	resData["cocktail_recipes"] = nil
+	// データ読み込み
 	for rows.Next() {
 		var cim models.CocktailRecipes
 		rows.Scan(&cim.Id, &cim.Name, &cim.Recipe, &cim.CocktailStyle, &cim.Alcohol)
-		cim_l = append(cim_l, cim)
+		resData["cocktail_recipes"] = append(resData["cocktail_recipes"], cim)
 	}
-	fmt.Printf("%v", cim_l)
-	// テスト中なので、適当な感じのものを返す
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
+	// デバック用
+	fmt.Printf("%v", resData)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return c.JSON(http.StatusOK, resData)
 }
 
 func ReturnHoge() string {
